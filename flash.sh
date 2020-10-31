9f7ef5465a1c9ccc545b6c4cace6b754c810b4b7
SergioOP-Tech <58672950+SergioOP-Tech@users.noreply.github.com>
2020-10-31 11:48:23 -0400

    Revert "Merge branch 'master' of https://github.com/commaai/eon-neos"
    This reverts commit f9ea3d57448e81bd2d24e0c03f97afd4bdeaa2a0, reversing
    changes made to 097b6176b9f39ac62457ea923972a942b607ce28.

--- flash.sh
+++ flash.sh
@@ -1,33 +1,19 @@
 #!/bin/bash -e
-
-FASTBOOT=platform-tools/fastboot
-
-VERSION="r28.0.2"
-PLATFORM="$(uname -s | tr '[:upper:]' '[:lower:]')"
-
-if [ ! -f $FASTBOOT ]; then
-    rm -rf platform-tools
-    rm -f platform-tools-latest-$PLATFORM.zip
-
-    curl -L https://dl.google.com/android/repository/platform-tools_$VERSION-$PLATFORM.zip --output platform-tools.zip
-    unzip platform-tools.zip
-    rm -f platform-tools.zip
-fi
-
-if [ ota-signed-latest.zip -nt files/system.img ]; then
+if [ ! -f system.simg ] || [ ota-signed-latest.zip -nt system.simg ]; then
   unzip -o ota-signed-latest.zip
-  touch files/system.img
+  echo "making system img sparse"
+  img2simg files/system.img system.simg
 fi
 
-sudo $FASTBOOT oem 4F500301 || true
-sudo $FASTBOOT flash recovery recovery.img
+sudo fastboot oem 4F500301 || true
+sudo fastboot flash recovery recovery.img
 
 # from OTA
-[ -f files/logo.bin ] && $FASTBOOT flash LOGO files/logo.bin
-sudo $FASTBOOT flash boot files/boot.img
-sudo $FASTBOOT flash system files/system.img
+[ -f files/logo.bin ] && fastboot flash LOGO files/logo.bin
+sudo fastboot flash boot files/boot.img
+sudo fastboot flash system system.simg
 
 # clear userdata
-sudo $FASTBOOT erase userdata
-sudo $FASTBOOT format cache
-sudo $FASTBOOT reboot
+sudo fastboot erase userdata
+sudo fastboot format cache
+sudo fastboot reboot
